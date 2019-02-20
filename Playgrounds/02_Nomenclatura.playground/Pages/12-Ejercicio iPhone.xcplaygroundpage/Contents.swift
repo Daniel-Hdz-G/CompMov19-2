c8de87929bb5f264b7b32ae3f1141406ec902837
Daniel Hernández <47311620+DanielHdzGvr@users.noreply.github.com>
2019-02-20 11:12:41 -0600

    Add files via upload

---
+++ Playgrounds/02_Nomenclatura.playground/Pages/12-Ejercicio iPhone.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+/*: 
+ ## Ejercicio: ¿Cuánto cabe en tu iPhone?
+ 
+ En este ejercicio, podrás obtener la respuesta a una pregunta habitual: ¿cuánto cabe en mi iPhone?
+ 
+ A diferencia de los ejercicios anteriores, no se proporcionará código. Sin embargo, debes tener en cuenta lo siguiente:
+ 
+ 
+ - La capacidad de almacenamiento del iPhone se mide en **gigabytes** (GB).
+ - El iPhone en cuestión tiene 8 GB de almacenamiento.
+ - Un gigabyte equivale a aproximadamente **1000 megabytes** (MB).
+ - El teléfono ya tiene **3 GB** de capacidad ocupada.
+ - **Un minuto** de video ocupa **150 MB** de almacenamiento.
+ 
+ - callout(Exercise):
+(Ejercicio):\
+¿Cuántos minutos de video ocuparían toda la capacidad del teléfono?\
+ _Pista_: Realiza todos los cálculos en megabytes (MB).
+*/
+let phoneStorageGb = 8
+let usedStorageGb = 3
+let remainingPhoneStorageGb = phoneStorageGb - usedStorageGb
+let remainingPhoneStorageMb = remainingPhoneStorageGb * 1000
+let minuteVideoStorageMb = 150
+let totalMinutesVideo = remainingPhoneStorageMb / minuteVideoStorageMb
+
+
+
+//:[Anterior](@previous)  |  Página 12 de 14  |  [Siguiente: Ejercicio: Prepararse por la mañana](@next)
